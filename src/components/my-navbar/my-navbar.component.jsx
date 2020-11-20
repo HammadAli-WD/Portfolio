ca2d557ed9caea7cad8ec38929eb5de494dc4554
Hammad Ali <63840111+HammadAli-WD@users.noreply.github.com>
2020-11-20 16:32:35 +0100

    Update

--- src/components/my-navbar/my-navbar.component.jsx
+++ src/components/my-navbar/my-navbar.component.jsx
@@ -26,7 +26,7 @@ const MyNavbar = () => {
               <Nav.Link href="#home">Home</Nav.Link>
               <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#skills">Skills</Nav.Link>
-              <Nav.Link href="#experience">Experience</Nav.Link>
+              <Nav.Link href="#academic">Academic</Nav.Link>
               <Nav.Link href="#projects">Projects</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
